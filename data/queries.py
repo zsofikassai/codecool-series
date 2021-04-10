401f184cf755c27cbc7dc5fe6536cfb9700ce959
zsofikassai <zsofiaszonja.kassai@gmail.com>
2021-04-10 20:30:43 +0200

    Get all shows ord by rating

--- data/queries.py
+++ data/queries.py
@@ -14,3 +14,14 @@ def get_most_rated():
     ORDER BY rating desc limit 15;
     """
     return data_manager.execute_select(query)
+
+def get_all_most_rated():
+    query = """SELECT title, year, runtime, round(rating, 1) as rating, 
+        string_agg(g.name, ',' order by name) as genres, trailer, homepage
+        FROM shows
+        LEFT JOIN show_genres sg on shows.id = sg.show_id
+        INNER JOIN genres g on g.id = sg.genre_id
+        GROUP BY title, year, runtime, rating, trailer, homepage 
+        ORDER BY rating desc;
+        """
+    return data_manager.execute_select(query)
