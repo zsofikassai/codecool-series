0196514a88419c5ddeedee7162900210aa8552ca
zsofikassai <zsofiaszonja.kassai@gmail.com>
2021-04-10 09:51:23 +0200

    Create new routes for passing data

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
 from data import queries
 import math
 from dotenv import load_dotenv
+
 
 load_dotenv()
 app = Flask('codecool_series')
@@ -11,6 +12,16 @@ def index():
     shows = queries.get_shows()
     return render_template('index.html', shows=shows)
 
+@app.route('/shows/most-rated')
+def most_rated():
+    headers = ['Title', 'Year', 'Runtime (min)', 'Rating', 'Genres', 'Trailer', 'Homepage']
+    most_rated=queries.get_most_rated()
+    return render_template('most-rated.html', shows=most_rated, headers = headers)
+
+@app.route('/get-shows')
+def get_shows():
+    shows = queries.get_shows()
+    return jsonify(shows)
 
 @app.route('/design')
 def design():
