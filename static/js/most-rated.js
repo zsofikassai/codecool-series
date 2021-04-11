ae120d9474ef02baed5e4c56d7e554f24199d68d
zsofikassai <zsofiaszonja.kassai@gmail.com>
2021-04-11 14:36:34 +0200

    Implement front-end sort

--- static/js/most-rated.js
+++ static/js/most-rated.js
@@ -24,23 +24,31 @@ function loadMostRated(){
     });
 }
 
+
+
 function showMostRated (most_rated) {
 
     const headers = ['Title', 'Year', 'Runtime (min)', 'Rating', 'Genres', 'Trailer', 'Homepage'];
-
+    const keys = ['title', 'year', 'runtime', 'rating', 'genres', 'trailer', 'homepage'];
     let showsContainer = document.querySelector('.table');
     const paginationElement = document.getElementById('pagination');
 
     let current_page = 1;
     let displayed_rows = 15;
 
-    function displayPagination(items, wrapper, displayed_rows, current_page){
-        let rows ='';
-        for (let header of headers){
-            rows += `<th>${header}</th>`;
-        }
 
-        wrapper.innerHTML ="";
+    function displayPagination(items, wrapper, displayed_rows, current_page) {
+        let rows = '';
+
+        for (let header of headers) {
+            let key = keys[headers.indexOf(header)];
+            rows += `<th id='clickable'>${header}</th>`;
+        }
+         /*   <form action="/get-most-rated" method="get">
+                                <input type="hidden" value="${key}" name ="sort">
+                                <button type="submit" class="sort"></button>
+                                </form> */
+        wrapper.innerHTML = "";
         current_page--;
 
         let loop_start = displayed_rows * current_page;
@@ -48,33 +56,78 @@ function showMostRated (most_rated) {
 
         for (let show of paginatedShows) {
             let row = '';
-            keys = ['title', 'year', 'runtime', 'rating', 'genres', 'trailer', 'homepage'];
-            for (let key of keys){
-                if (key == 'title'){
+            for (let key of keys) {
+                if (key == 'title') {
                     row += `
                 <td> <a href="/show/${show.id}"> ${show[key]} </a></td>
             `;
-                }
-                else if (show[key] != null){
+                } else if (show[key] != null) {
                     row += `
                 <td> ${show[key]}</td>
             `;
-                }
-                else{
+                } else {
                     row += `
                 <td> No Url</td>
             `;
                 }
 
             }
-            rows +=`<tr>
+            rows += `<tr>
                 ${row}
                </tr>`;
         }
         wrapper.insertAdjacentHTML("beforeend", rows);
-
-
+        setupSort(items);
     }
+
+    function setupSort(items){
+        let columnHeaders = document.querySelectorAll('#clickable');
+
+        for (let i=0; i<columnHeaders.length; i++){
+            columnHeaders[i].toggleAttribute('desc')
+            columnHeaders[i].addEventListener('toggle',  columnHeaders[i] )
+            columnHeaders[i].addEventListener('click', function(){
+                const headers = ['Title', 'Year', 'Runtime (min)', 'Rating', 'Genres', 'Trailer', 'Homepage'];
+                const keys = ['title', 'year', 'runtime', 'rating', 'genres', 'trailer', 'homepage'];
+                let key = keys[headers.indexOf(columnHeaders[i].innerHTML)];
+                if (this.hasAttribute('desc')) {
+                    sortByColumn(items, key, 'desc') ;}
+                else{
+                    sortByColumn(items, key, 'asc');
+                }
+            })
+
+        }
+    }
+
+    function sortByColumn(items, column, order){
+        function compareValues(key, order) {
+            return function innerSort(a, b) {
+                if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+                    // property doesn't exist on either object
+                    return 0;
+                }
+
+                const varA = (typeof a[key] === 'string')
+                    ? a[key].toUpperCase() : a[key];
+                const varB = (typeof b[key] === 'string')
+                    ? b[key].toUpperCase() : b[key];
+
+                let comparison = 0;
+                if (varA > varB) {
+                    comparison = 1;
+                } else if (varA < varB) {
+                    comparison = -1;
+                }
+                return (
+                    (order === 'desc') ? (comparison * -1) : comparison
+                );
+            };
+        }
+        displayPagination(items.sort(compareValues(column, order)), showsContainer, displayed_rows, current_page);
+    }
+
+
 
     function setupPagination(items, wrapper, displayed_rows) {
         wrapper.innerHTML = "";
@@ -82,8 +135,8 @@ function showMostRated (most_rated) {
         let prevButton = document.createElement('button');
         prevButton.innerText = '<<';
         wrapper.appendChild(prevButton);
-        prevButton.addEventListener('click', function(){
-            if (current_page != 1){
+        prevButton.addEventListener('click', function () {
+            if (current_page != 1) {
                 current_page--;
             }
             displayPagination(most_rated, showsContainer, displayed_rows, current_page);
@@ -95,9 +148,9 @@ function showMostRated (most_rated) {
         }
         let nextButton = document.createElement('button');
         nextButton.innerText = '>>';
-        nextButton.addEventListener('click', function(){
-            if (current_page != page_count){
-                 current_page++;
+        nextButton.addEventListener('click', function () {
+            if (current_page != page_count) {
+                current_page++;
             }
 
             displayPagination(most_rated, showsContainer, displayed_rows, current_page);
@@ -105,20 +158,28 @@ function showMostRated (most_rated) {
         wrapper.appendChild(nextButton);
     }
 
-    function paginationButton(most_rated, page){
+    function paginationButton(most_rated, page) {
         let button = document.createElement('button');
         button.innerText = page;
-        button.addEventListener('click', function (){
+        button.addEventListener('click', function () {
             current_page = page;
             displayPagination(most_rated, showsContainer, displayed_rows, current_page);
         })
-        if (current_page == page) {button.classList.add('active')};
+        if (current_page == page) {
+            button.classList.add('active')
+        }
+        ;
         return button;
     }
 
-        displayPagination(most_rated, showsContainer, displayed_rows, current_page);
-        setupPagination(most_rated, paginationElement, displayed_rows);
-    }
+    displayPagination(most_rated, showsContainer, displayed_rows, current_page);
+    setupPagination(most_rated, paginationElement, displayed_rows);
+}
+/*   let sortBtns = document.querySelectorAll('.sort');
+        for (let i=0; i<sortBtns.length; i++){
+            sortBtns[i].addEventListener('click', function(){
+                console.log('mukodik');
+            })}*/
 
 
 
